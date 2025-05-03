65fe95e6ff976863f37ec7062fb817c9c67e3558
MAHESH <maheshshinde9100@gmail.com>
2025-05-03 22:23:33 +0530

    initial commit for mentee_connect_frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <main className="p-4">
+                <h2 className="text-xl font-bold text-gray-800">Welcome to MenteeConnect</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
